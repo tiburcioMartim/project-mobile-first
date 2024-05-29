0921d5106e38f379cce8bdd9439f8c4977c58ec6
tiburcioMartim <tiburciomartim@gmail.com>
2024-05-29 06:01:43 -0300

    Refatoration in code

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -1,6 +1,11 @@
 const menuHamburguer = document.querySelector('.menu-toggle');
 const asidContainer = document.querySelector('.aside-container');
+const btnSpanClose = document.querySelector('.btn-span-close');
 
 menuHamburguer.addEventListener('click', () => {
+    asidContainer.classList.toggle('open');
+});
+
+btnSpanClose.addEventListener('click', () => {
     asidContainer.classList.toggle('open');
 });
